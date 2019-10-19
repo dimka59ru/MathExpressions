a2953c2c35e3de52d83225dbd2822e49ab7e59b9
Dmitriy <dimka59ru@gmail.com>
2019-10-20 00:52:17 +0500

    Добавил бд и подключение к ней

---
+++ MathExpressions/Models/FormulaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpressions.Models
+{
+    public class FormulaModel
+    {
+        public int Id { get; set; }
+        public int Infix { get; set; }
+    }
+}
